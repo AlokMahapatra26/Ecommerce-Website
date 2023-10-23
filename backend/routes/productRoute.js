d486dd2cd8ec5582103a95bed87eecc6ee2b8c91
AlokMahapatra26 <alokmahapatra2604@gmail.com>
2023-10-24 03:27:05 +0530

    created update product func and added in route

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { getAllProducts  , createProduct} = require('../controller/productController');
+const { getAllProducts  , createProduct, updateProduct} = require('../controller/productController');
 
 const router = express.Router();
 
 router.route("/products").get(getAllProducts);
-router.route("/new").post(createProduct);
+router.route("/product/new").post(createProduct);
+router.route("/product/:id").put(updateProduct);
 
 
 module.exports = router;
