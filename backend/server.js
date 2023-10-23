ae7ea1fdfecd3a82ba8b8769e257e7d1accc6132
AlokMahapatra26 <alokmahapatra2604@gmail.com>
2023-10-23 17:24:45 +0530

    database conencted

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,9 @@
 const app = require("../backend/app");
+const connectDatabase = require("./database/database");
 
+
+//Connecting Database
+connectDatabase()
 
 app.get("/" , (req,res)=>{
     res.send("Hello World")
