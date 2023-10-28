2c5cf104f86afffbed7013a5de726046adf70bdc
AlokMahapatra26 <alokmahapatra2604@gmail.com>
2023-10-28 15:10:18 +0530

    JWT created

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const userSchema = mongoose.Schema({
     name:{
@@ -46,4 +47,12 @@ userSchema.pre("save",async function(next){
     }
     this.password = await bcrypt.hash(this.password,10)
 })
+
+//JWT TOKEN
+userSchema.methods.getJWTToken = function (){
+    return jwt.sign({id:this._id},"jsdjfhsidfjh32423h4982hwe98fhn38hr34nf3948fh38f83h" , {
+        expiresIn:5
+    })
+}
+
 module.exports = mongoose.model("User",userSchema)
