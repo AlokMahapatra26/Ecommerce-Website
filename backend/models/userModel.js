86e87289545be18aae3f723348a8c2caedf31800
AlokMahapatra26 <alokmahapatra2604@gmail.com>
2023-10-28 13:19:43 +0530

    password hashing applied

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = mongoose.Schema({
     name:{
@@ -38,5 +39,11 @@ const userSchema = mongoose.Schema({
     resetPasswordExpire:Date
 })
 
-console.log("suceess")
+userSchema.pre("save",async function(next){
+
+    if(!this.isModified("password")){
+        next()
+    }
+    this.password = await bcrypt.hash(this.password,10)
+})
 module.exports = mongoose.model("User",userSchema)
