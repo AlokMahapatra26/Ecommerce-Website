0b4907225abda10b69ad0d3e35eca7e7a6860ae1
AlokMahapatra26 <alokmahapatra2604@gmail.com>
2023-10-30 01:01:01 +0530

    created authority func of accessing API

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const { getAllProducts  , createProduct, updateProduct, deleteProduct, getProductDetails} = require('../controller/productController');
-const { isAuthenticateUser } = require('../middleware/auth');
+const { isAuthenticateUser , authorizeRoles} = require('../middleware/auth');
 
 const router = express.Router();
 
 router.route("/products").get(getAllProducts);
-router.route("/product/new").post(isAuthenticateUser , createProduct);
-router.route("/product/:id").put(isAuthenticateUser , updateProduct).delete(isAuthenticateUser ,deleteProduct).get(getProductDetails);
+router.route("/product/new").post(isAuthenticateUser , authorizeRoles("admin") , createProduct);
+router.route("/product/:id").put(isAuthenticateUser , authorizeRoles("admin") , updateProduct).delete(isAuthenticateUser , authorizeRoles("admin"),deleteProduct).get(getProductDetails);
 
 
 module.exports = router;
