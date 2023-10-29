60b391e8c5bb2825b6acf34a5c59bb862e7df4d1
AlokMahapatra26 <alokmahapatra2604@gmail.com>
2023-10-30 00:32:58 +0530

    created logout feature

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -45,3 +45,16 @@ exports.loginUser = catchAsyncError(async(req,res,next)=>{
 })
 
 //Logout User
+
+exports.logout = catchAsyncError(async(req,res,next)=>{
+
+    res.cookie("token", null , {
+        expires:new Date(Date.now()),
+        httpOnly:true
+    })
+
+    res.status(200).json({
+        success:true,
+        message:"logged out succesfully"
+    })
+})
