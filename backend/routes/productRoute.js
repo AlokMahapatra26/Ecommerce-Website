f007d5a6576660cc886ccd5973c4de9cd48c46ee
AlokMahapatra26 <alokmahapatra2604@gmail.com>
2023-11-02 03:15:41 +0530

    minor syntax fixes

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -5,8 +5,15 @@ const { isAuthenticateUser , authorizeRoles} = require('../middleware/auth');
 const router = express.Router();
 
 router.route("/products").get(getAllProducts);
-router.route("/product/new").post(isAuthenticateUser , authorizeRoles("admin") , createProduct);
-router.route("/product/:id").put(isAuthenticateUser , authorizeRoles("admin") , updateProduct).delete(isAuthenticateUser , authorizeRoles("admin"),deleteProduct).get(getProductDetails);
+
+router.route("/admin/product/new").post(isAuthenticateUser , authorizeRoles("admin") , createProduct);
+
+router
+    .route("/admin/product/:id")
+    .put(isAuthenticateUser , authorizeRoles("admin") , updateProduct)
+    .delete(isAuthenticateUser , authorizeRoles("admin"),deleteProduct)
+
+    router.route("/product/:id").get(getProductDetails);
 
 
 module.exports = router;
